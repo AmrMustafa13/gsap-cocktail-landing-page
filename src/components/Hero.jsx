0a7c4ae9db86606223396d79637c2306dd667511
AmrMustafa13 <amrmostafa1311@gmail.com>
2025-06-24 00:52:18 +0300

    Refactor Hero component to include video background and enhance animation timing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
+  const videoRef = useRef(null);
+
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+
   useGSAP(() => {
     const heroSplit = new SplitText(".title", {
       type: "chars, words",
@@ -13,23 +18,22 @@ const Hero = () => {
       type: "lines",
     });
 
-    heroSplit.chars.forEach((char) => {
-      char.classList.add("text-gradient");
-    });
+    // Apply text-gradient class once before animating
+    heroSplit.chars.forEach((char) => char.classList.add("text-gradient"));
 
     gsap.from(heroSplit.chars, {
       yPercent: 100,
       duration: 1.8,
-      stagger: 0.05,
       ease: "expo.out",
+      stagger: 0.06,
     });
 
     gsap.from(paragraphSplit.lines, {
       opacity: 0,
       yPercent: 100,
       duration: 1.8,
-      stagger: 0.05,
       ease: "expo.out",
+      stagger: 0.06,
       delay: 1,
     });
 
@@ -42,20 +46,28 @@ const Hero = () => {
           scrub: true,
         },
       })
-      .to(
-        ".right-leaf",
-        {
-          y: 200,
-        },
-        0
-      )
-      .to(
-        ".left-leaf",
-        {
-          y: -200,
-        },
-        0
-      );
+      .to(".right-leaf", { y: 200 }, 0)
+      .to(".left-leaf", { y: -200 }, 0)
+      .to(".arrow", { y: 100 }, 0);
+
+    const startValue = isMobile ? "top 50%" : "center 60%";
+    const endValue = isMobile ? "120% top" : "bottom top";
+
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: "video",
+        start: startValue,
+        end: endValue,
+        scrub: true,
+        pin: true,
+      },
+    });
+
+    videoRef.current.onloadedmetadata = () => {
+      tl.to(videoRef.current, {
+        currentTime: videoRef.current.duration,
+      });
+    };
   }, []);
 
   return (
@@ -91,6 +103,15 @@ const Hero = () => {
           </div>
         </div>
       </section>
+      <div className="video absolute inset-0">
+        <video
+          src="/videos/output.mp4"
+          preload="auto"
+          muted
+          playsInline
+          ref={videoRef}
+        />
+      </div>
     </>
   );
 };
